37b0b710fa3e5068f9c672eb854ef917087b09e6
atroopa <atroopa@gmail.com>
2021-12-04 12:04:58 +0330

    OnBoardingOne class create

--- lib/onboarding_one.dart
+++ lib/onboarding_one.dart
@@ -1,2 +1,12 @@
 import 'package:flutter/material.dart';
 
+class OnBoardingOne extends StatelessWidget {
+  const OnBoardingOne({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
