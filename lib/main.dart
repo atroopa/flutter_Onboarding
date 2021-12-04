9b60292c16ab5779ab9afe7fcb0adbde1e5c3a0f
atroopa <atroopa@gmail.com>
2021-12-04 12:23:02 +0330

    Column padding now have argument

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:OnboardingApp/onboarding_one.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 void main() => runApp(const MyApp());
@@ -16,10 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home:Scaffold(
-        appBar: AppBar(
-          title: Text("My Fucking Project"),  
-        ),
-        body: Center(child: Text("FuckBody"),),
+        body: OnBoardingOne(),
       )
     );
   }
--- lib/onboarding_one.dart
+++ lib/onboarding_one.dart
@@ -5,8 +5,19 @@ class OnBoardingOne extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(24.4, 42.0, 24.4, 42.0),
+              child: Row(
+                children: [],
+              ),
+            ),
+          ],
+        ) 
+        ),
     );
   }
 }
